005d39fb72079deab302c1176f29e1a4305b7606
Hunter Barton <hunter@Hunters-MacBook-Pro.local>
2022-03-01 17:41:54 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  #app
+    map-leaflet
+</template>
+
+<script>
+import MapLeaflet from './components/molecules/Map.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MapLeaflet
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0px;
+  padding: 0px;
+}
+</style>
